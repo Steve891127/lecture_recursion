ed69b63f8069987ee41872aba7c320826a211483
Stepan Adam <271025@vutbr.cz>
2025-04-28 09:35:00 +0200

    fibo1

--- fibo.py
+++ fibo.py
@@ -1,4 +1,12 @@
-
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+         output = recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+         return output
+print(recursive_nth_fibo(5))
 def main():
     pass
 
